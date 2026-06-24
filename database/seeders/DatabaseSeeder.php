ca29551d9fb6a3575b4f775d2f35af57fd619f34
alexia <skymoonlexi@gmail.com>
2026-06-24 16:49:51 -0300

    Primer commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Pet;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Pet::factory(50)->create();
+
+        
+    }
+}
---
+++ resources/views/pets/create.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Nueva Mascota</title>
+</head>
+<body>
+
+    <h1>Registrar Nueva Mascota</h1>
+
+    <form action="{{ route('pets.store') }}" method="POST">
+        @csrf
+
+        <div>
+            <label>Nombre:</label>
+            <input type="text" name="name" required>
+        </div>
+
+        <br>
+
+        <div>
+            <label>Especie:</label>
+            <input type="text" name="species" required>
+        </div>
+
+        <br>
+
+        <div>
+            <label>Edad:</label>
+            <input type="number" name="age" required>
+        </div>
+
+        <br>
+
+        <button type="submit">
+            Guardar Mascota
+        </button>
+
+    </form>
+
+    <br>
+
+    <a href="{{ route('pets.index') }}">
+        Volver a la lista
+    </a>
+
+</body>
+</html>
